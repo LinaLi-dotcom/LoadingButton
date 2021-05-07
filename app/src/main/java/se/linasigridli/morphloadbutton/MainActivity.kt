5d61621c0494f48f8c3cfb5b5813382c132aac6b
lina <linasigridli@gmail.com>
2021-05-07 13:08:48 +0200

    button kör animation men reverta inte

--- app/src/main/java/se/linasigridli/morphloadbutton/MainActivity.kt
+++ app/src/main/java/se/linasigridli/morphloadbutton/MainActivity.kt
@@ -1,26 +1,63 @@
 package se.linasigridli.morphloadbutton
 
+import android.animation.ValueAnimator
 import android.content.Context
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import br.com.simplepass.loadingbutton.animatedDrawables.ProgressType
 import br.com.simplepass.loadingbutton.customViews.CircularProgressButton
+import br.com.simplepass.loadingbutton.customViews.ProgressButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn: CircularProgressButton = findViewById<Button>(R.id.btn_Download) as CircularProgressButton
-        btn.startAnimation()
-        //btn.doneLoadingAnimation(fillColor, bitmap);
-        btn.revertAnimation()
+        val btn: CircularProgressButton = findViewById<Button>(R.id.btn_download) as CircularProgressButton
+
+        btn.run {
+            setOnClickListener {
+                morphDoneAndRevert(this@MainActivity)
+            }
+        }
     }
-
-
-
-
-
-
-
 }
+
+private fun defaultColor(context: Context) = ContextCompat.getColor(context, android.R.color.holo_green_light)
+
+private fun defaultDoneImage(resources: Resources) =
+        BitmapFactory.decodeResource(resources, R.drawable.ic_baseline_done_24)
+
+private fun ProgressButton.morphDoneAndRevert(
+        context: Context,
+        fillColor: Int = defaultColor(context),
+        bitmap: Bitmap = defaultDoneImage(context.resources),
+        doneTime: Long = 3000,
+        revertTime: Long = 4000
+) {
+        progressType = ProgressType.INDETERMINATE
+        startAnimation()
+        Handler().run {
+            postDelayed({ doneLoadingAnimation(fillColor, bitmap) }, doneTime)
+            postDelayed(::revertAnimation, revertTime)
+        }
+   }
+
+private fun progressAnimator(progressButton: ProgressButton) = ValueAnimator.ofFloat(0F, 100F).apply {
+    duration = 1500
+    startDelay = 500
+    addUpdateListener { animation ->
+        progressButton.setProgress(animation.animatedValue as Float)
+    }
+}
+
+
+
